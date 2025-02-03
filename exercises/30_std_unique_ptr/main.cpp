a2a93a37aba54d032ee63b2f9f258094df6dcdac
revisit <1973004703@qq.com>
2025-02-04 02:34:43 +0800

    更新

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -53,20 +53,13 @@ int main(int argc, char **argv) {
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
         // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"d"},
-        {"r", "d", "d"},
+        {"d", "ffr"},
+        {"d", "d", "r"},
     };
 
-    // ---- 不要修改以下代码 ----
-    for (auto i = 0; i < 3; ++i) {
-        for (auto j = 0; j < problems[i].size(); ++j) {
-            printf("problems[%d][%d] = %s\n", i, j, problems[i][j].c_str());
-        }
-    }
     for (auto i = 0; i < 3; ++i) {
         ASSERT(problems[i].size() == answers[i].size(), "wrong size");
         for (auto j = 0; j < problems[i].size(); ++j) {
-            printf("problems[%d][%d] = %s\n", i, j, problems[i][j].c_str());
             ASSERT(std::strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
         }
     }
